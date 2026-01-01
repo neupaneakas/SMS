6d5809ba350ba17d8f7788b674de1a98156c9a48
Aakash Neupane <neupaneakash687@gmail.com>
2026-01-01 19:27:08 +0545

    Fixed some of the buttons not working like adding student & courses

--- student_mgmt/backend/core/forms.py
+++ student_mgmt/backend/core/forms.py
@@ -1,17 +1,26 @@
 from django import forms
 from .models import Student, Course, Enrollment
 
-class StudentForm(forms.ModelForm):
+class BaseForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.Select):
+                field.widget.attrs['class'] = 'form-select'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+class StudentForm(BaseForm):
     class Meta:
         model = Student
         fields = ['first_name', 'last_name', 'student_id', 'email']
 
-class CourseForm(forms.ModelForm):
+class CourseForm(BaseForm):
     class Meta:
         model = Course
         fields = ['name', 'code', 'teacher']
 
-class EnrollmentForm(forms.ModelForm):
+class EnrollmentForm(BaseForm):
     class Meta:
         model = Enrollment
         fields = ['student', 'course', 'grade']
